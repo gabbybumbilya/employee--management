5c9c3a7ab898652f2a708ebd16f0a32de141b1dc
ChuckNorris304 <gabbybumbilya@gmail.com>
2025-10-21 09:15:26 +0800

    updated files

--- db_connection.php
+++ db_connection.php
@@ -4,10 +4,9 @@ $dbname = 'if0_40216671_employee_management';
 $username = 'if0_40216671';
 $password = 'fkyyqGcV80YTYAs';
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
